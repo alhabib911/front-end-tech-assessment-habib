e9f38eaf2b4ca5930b5b1a507d47e74547683de8
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-30 18:51:10 +0600

    responsive done

--- src/Component/Event/Event.js
+++ src/Component/Event/Event.js
@@ -1,32 +1,45 @@
 import React from 'react';
 import { ImCalendar, ImLocation2 } from 'react-icons/im';
+import { MdArrowForwardIos } from 'react-icons/md';
 import Cake from '../../Image/Birthdaycake.png'
 import '../Style/Style.css'
 
 const Event = () => {
     return (
-        <div>
-            <div>
+        <div className='event-page'>
+            <div className='event-content'>
                 <div>
                     <h1>Birthday Bash</h1>
-                    <small>Hosted by Elysia</small>
+                    <small>Hosted by <span className='host'>Elysia</span></small>
                 </div>
                 <div>
-                    <div>
-                        <ImCalendar />
+                    <div className="arrow">
+                        <div className='calender'>
+                            <div className='calender-icon'>
+                                <ImCalendar />
+                            </div>
+                            <div className='calender-content'>
+                                <h4 className='date-time'>18 August 6:00PM</h4>
+                                <p>to <span className='time-date'>19 August 1:00PM</span> UTC +10</p>
+                            </div>
+                        </div>
+                        <div className='arrow-icon'>
+                            <MdArrowForwardIos />
+                        </div>
                     </div>
-                    <div>
-                        <p>18 August 6:00PM</p>
-                        <p>to 19 August 1:00PM UTC +10</p>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <ImLocation2/>
-                    </div>
-                    <div>
-                        <p>Street name</p>
-                        <p>Suburb, State, Postcode</p>
+                    <div className="arrow">
+                        <div className='location'>
+                            <div className='location-icon'>
+                                <ImLocation2 />
+                            </div>
+                            <div className='location-content'>
+                                <h4>Street name</h4>
+                                <p>Suburb, State, Postcode</p>
+                            </div>
+                        </div>
+                        <div className='arrow-icon'>
+                            <MdArrowForwardIos />
+                        </div>
                     </div>
                 </div>
             </div>
